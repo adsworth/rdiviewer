395b482b40ca96aa945f11b7fb8544ee4b253168
Adi Sieker <adi@sieker.info>
2008-11-30 14:50:50 +0100

    moved the App class - moved the App class to it's new location rdiviewer.gui.apps

--- rdiviewer.py
+++ rdiviewer.py
@@ -22,33 +22,8 @@ import sys
 
 import wx
 
-from rdiviewer.gui.frames import MainFrame
-import rdiviewer.common as common
-
-class advApp(wx.App):
-    def OnInit(self):
-        self.SetAppName('advViewer')
-        ini_filename = self.GetAppName().lower() + '.ini'
-        self.config = wx.FileConfig( localFilename=ini_filename)
-        self.config.SetRecordDefaults(True)
-        wx.ConfigBase_Set(self.config)
-
-        win_pos = wx.Point(self.config.ReadInt(common.INI_WINDOW_POS_X, 100),
-                           self.config.ReadInt(common.INI_WINDOW_POS_Y, 100)
-                           )
-        win_size = wx.Size(self.config.ReadInt(common.INI_WINDOW_SIZE_W, 600),
-                           self.config.ReadInt(common.INI_WINDOW_SIZE_H, 600)
-                           )
-
-        self.frame = MainFrame(None, -1, "advViewer",pos=win_pos, size=win_size)
-        self.frame.Show(True)
-
-        if len(sys.argv) > 1:
-            self.frame.LoadFile(sys.argv[1])
-
-        return True
-
+from rdiviewer.gui.apps import MainApp
 
 if __name__ == '__main__':
-    app = advApp(False)
+    app = MainApp(False)
     app.MainLoop()
--- rdiviewer/gui/apps.py
+++ rdiviewer/gui/apps.py
@@ -16,4 +16,33 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+import sys
 
+import wx
+
+from rdiviewer import common
+from rdiviewer.gui.frames import MainFrame
+import rdiviewer.common as common
+
+class MainApp(wx.App):
+    def OnInit(self):
+        self.SetAppName('RDIViewer')
+        ini_filename = self.GetAppName().lower() + '.ini'
+        self.config = wx.FileConfig( localFilename=ini_filename)
+        self.config.SetRecordDefaults(True)
+        wx.ConfigBase_Set(self.config)
+
+        win_pos = wx.Point(self.config.ReadInt(common.INI_WINDOW_POS_X, 100),
+                           self.config.ReadInt(common.INI_WINDOW_POS_Y, 100)
+                           )
+        win_size = wx.Size(self.config.ReadInt(common.INI_WINDOW_SIZE_W, 600),
+                           self.config.ReadInt(common.INI_WINDOW_SIZE_H, 600)
+                           )
+
+        self.frame = MainFrame(None, -1, "RDIViewer",pos=win_pos, size=win_size)
+        self.frame.Show(True)
+
+        if len(sys.argv) > 1:
+            self.frame.LoadFile(sys.argv[1])
+
+        return True
